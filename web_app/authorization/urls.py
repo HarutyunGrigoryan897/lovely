68592722c2e8d7d37adc8dc15209eab7d67a2f1e
Gevgor87 <gevgor87@gmail.com>
2025-09-02 22:58:16 +0400

    Admin logic, User approve

--- web_app/authorization/urls.py
+++ web_app/authorization/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from authorization.views import CustomUserCreateView, CustomUserRetrieveView
+from authorization.views import (CustomUserCreateView, CustomUserRetrieveView, CheckAdminView,
+                                 AdminListView, ApproveUserView, RejectUserView)
 
 urlpatterns = [
     path("users/", CustomUserCreateView.as_view(), name="user-create"),
     path("user-info/", CustomUserRetrieveView.as_view(), name="user-info"),
+    path("check-admin/", CheckAdminView.as_view(), name="check-admin"),
+    path("all-admins/", AdminListView.as_view(), name="all-admins"),
+    path("user-approve/<int:telegram_id>/", ApproveUserView.as_view(), name="approve-user"),
+    path("user-reject/<int:telegram_id>/", RejectUserView.as_view(), name="reject-user"),
 ]
--- web_app/authorization/views.py
+++ web_app/authorization/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics, status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from authorization.models import CustomUser
@@ -26,3 +27,46 @@ class CustomUserRetrieveView(generics.GenericAPIView):
             return Response({"detail": "User not found."}, status=404)
         except Exception:
             return Response({"detail": "Something went wrong."}, status=500)
+        
+class CheckAdminView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        telegram_id = request.query_params.get("telegram_id")
+        if not telegram_id:
+            return Response({"detail": "telegram_id query parameter is required."}, status=400)
+
+        try:
+            user = CustomUser.objects.get(telegram_id=telegram_id)
+            return Response({"is_admin": user.is_superuser}, status=status.HTTP_200_OK)
+        except CustomUser.DoesNotExist:
+            return Response({"detail": "User not found."}, status=404)
+        except Exception:
+            return Response({"detail": "Something went wrong."}, status=500)
+
+class AdminListView(APIView):
+
+    def get(self, request):
+        admins = CustomUser.objects.filter(is_superuser=True).values("telegram_id", "username", "first_name", "last_name")
+        return Response(list(admins))
+    
+class ApproveUserView(APIView):
+    def post(self, request, telegram_id):
+        try:
+            user = CustomUser.objects.get(telegram_id=telegram_id)
+            user.approved = True
+            user.save()
+            return Response({"detail": "User approved successfully."})
+        except CustomUser.DoesNotExist:
+            return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+
+
+class RejectUserView(APIView):
+    def post(self, request, telegram_id):
+        try:
+            user = CustomUser.objects.get(telegram_id=telegram_id)
+            user.approved = False
+            user.save()
+            return Response({"detail": "User rejected successfully."})
+        except CustomUser.DoesNotExist:
+            return Response({"detail": "User not found."}, status=status.HTTP_404_NOT_FOUND)
